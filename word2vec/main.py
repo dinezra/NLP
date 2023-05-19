89db1cdaa995cb0529def60fed38476ef38e7b2b
dine <din.ezra@nice.com>
2023-05-19 14:18:12 +0300

    fix bugs

--- word2vec/main.py
+++ word2vec/main.py
@@ -15,17 +15,3 @@ sentences = normalize_text(path_data)
 model = SkipGram(sentences)
 model.learn_embeddings(model_path=model_path,
                        epochs=100)
-
-# V = model.combine_vectors(model.T,
-#                           model.C,
-#                           combo=combo_num,
-#                           model_path=vec_path)
-
-# model = load_model(os.path.join(model_path,"final_model.pickle"))
-# print("play", "ball", model.compute_similarity("play", "ball"))
-
-# print(model.find_analogy('cold',
-#                         'play',
-#                         'ball')
-#       )
-
